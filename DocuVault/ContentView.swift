6280ae577750917778e38addbcb93cd374403277
Anirudh Venkatachalam <50367124+anirudhvee@users.noreply.github.com>
2025-04-19 14:55:55 -0700

    initial commit

---
+++ DocuVault/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DocuVault
+//
+//  Created by Anirudh Venkatachalam on 4/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DocuVault/DocuVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocuVaultApp.swift
+//  DocuVault
+//
+//  Created by Anirudh Venkatachalam on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct DocuVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
